498c47cd61b9629910a6ef448c14be9f11933a03
ianlingat <arthlingat@gmail.com>
2023-08-15 02:29:15 -0400

    Updated Ansible System Values Comparison Aug 5, 2023 2:29 PM

---
+++ dir_creator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Christian Arth Lingat
+#System Value Checking Directory Creator
+cd /var/lib/awx/projects/AS400_PTF_PROJECT
+mkdir -pv dsv/{motherfile,fetch_system_values}
+
---
+++ dir_remover.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Christian Arth Lingat
+#System Value Checking Directory Creator
+cd /var/lib/awx/projects/AS400_PTF_PROJECT
+rm -rvf dsv
+
