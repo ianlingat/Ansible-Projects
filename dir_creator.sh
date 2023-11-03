41f83862f7520033b6e52421a3a68ebf683ebd98
ianlingat <arthlingat@gmail.com>
2023-11-03 02:52:02 -0400

    Updated Playbooks

--- dir_creator.sh
+++ dir_creator.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Christian Arth Lingat
 #System Value Checking Directory Creator
-cd /var/lib/awx/projects/AS400_PTF_PROJECT
 mkdir -pv dsv/{motherfile,fetch_system_values}
-
--- dir_remover.sh
+++ dir_remover.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Christian Arth Lingat
 #System Value Checking Directory Creator
-cd /var/lib/awx/projects/AS400_PTF_PROJECT
 rm -rvf dsv
-
